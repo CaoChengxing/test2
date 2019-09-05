cce3b72dd45bb5f948196c9010242213884cfb5e
ccx <Caocx@biji.com>
2019-09-05 19:33:19 +0800

    第一次提交

---
+++ src/main/java/test6/Test.java
@@ -0,0 +1,13 @@
+package test6;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("第一次");
+	}
+	
+}
